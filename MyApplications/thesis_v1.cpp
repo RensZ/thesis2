14e38d47de77d783c3aadf9620d992f9ad6184ba
RensZ <rensvanderzwaard@gmail.com>
2020-02-21 17:13:05 +0100

    2/21 messed around a bit today

--- MyApplications/thesis_v1.cpp
+++ MyApplications/thesis_v1.cpp
@@ -11,7 +11,7 @@
  */
 
 #include <Tudat/SimulationSetup/tudatSimulationHeader.h>
-
+#include <chrono>
 
 // Get path for output directory.
 namespace tudat_applications
@@ -27,8 +27,6 @@ namespace tudat_applications
         std::string outputPath = filePath_.substr( 0, filePath_.length( )
                                     - std::string( "MyApplications" ).length( ) )
                                     + std::string( "/" );
-
-        //std::string outputPath = reducedPath + "thesis_v1.cpp";
         if( extraDirectory != "" ){outputPath += extraDirectory;}
         if( outputPath.at( outputPath.size( ) - 1 ) != '/' ){outputPath += "/";}
 
@@ -37,7 +35,7 @@ namespace tudat_applications
 }
 
 
-// convert calendar date and time to seconds after J2000 (the input format for TUDAT)
+// convert calendar date and time to seconds after J2000
 double secondsAfterJ2000(Eigen::Vector6i datetime){
     using namespace tudat;
     using namespace tudat::basic_astrodynamics;
@@ -51,17 +49,26 @@ double secondsAfterJ2000(Eigen::Vector6i datetime){
 
 int main( )
 {
+
     // Define namespaces
     using namespace tudat;
-    using namespace tudat::ephemerides;
+    using namespace tudat::observation_models;
+    using namespace tudat::orbit_determination;
+    using namespace tudat::estimatable_parameters;
     using namespace tudat::interpolators;
     using namespace tudat::numerical_integrators;
     using namespace tudat::spice_interface;
     using namespace tudat::simulation_setup;
-    using namespace tudat::basic_astrodynamics;
-    using namespace tudat::basic_mathematics;
     using namespace tudat::orbital_element_conversions;
+    using namespace tudat::ephemerides;
     using namespace tudat::propagators;
+    using namespace tudat::basic_astrodynamics;
+    using namespace tudat::coordinate_conversions;
+    using namespace tudat::ground_stations;
+    using namespace tudat::observation_models;
+    using namespace tudat::statistics;
+    using namespace tudat::basic_mathematics;
+    using namespace tudat::unit_conversions;
 
 
     /////////////////////
@@ -73,16 +80,23 @@ int main( )
     initialTime << 2020, 1, 1, 0, 0, 0; // YYYY, MM, DD, hh, mm, ss
     finalTime   << 2020, 2, 1, 0, 0, 0; // YYYY, MM, DD, hh, mm, ss
 
-    // Integration step size
-    double integrationStepSize = 3600.0; //seconds
+    // Planet propagation choices
+    bool propogatePlanets = false; // Propogate the other planets besides Mercury
+    bool useDirectSpice = true; // Use direct SPICE (more accurate than tbulated Spice)
 
     // Sun inputs
     double sunRadius = 695.7E6; //m, from nasa fact sheet
     double sunJ2 = 2.0E-7; //from Hamid
     double sunGravitationalParameter = 132712E6; //km3/s2, from nasa fact sheet
 
-    // Propogate planets besides Mercury
-    bool propogatePlanets = true;
+    // ABM integrator settings
+    double initialTimeStep = 3600;
+    double minimumStepSize = 3600/8;
+    double maximumStepSize = 3600*8;
+    double relativeErrorTolerence = 10E-12;
+    double absoluteErrorTolerence = 10E-12;
+    int minimumOrder = 6;
+    int maximumOrder = 12;
 
 
 
@@ -94,11 +108,10 @@ int main( )
     std::cout << "building environment..." << std::endl;
 
     // initial and final time to Julian
-
     double initialEphemerisTime = secondsAfterJ2000(initialTime);
     double finalEphemerisTime = secondsAfterJ2000(finalTime);
 
-    //Load spice kernels.
+    // Load spice kernels.
     std::string kernelsPath = input_output::getSpiceKernelPath( );
     spice_interface::loadStandardSpiceKernels( );
 
@@ -116,9 +129,19 @@ int main( )
     bodyNames[ 7 ] = "Moon";
 
     // Default body settings
-    double buffer = 5*integrationStepSize; //see Tudat libraries 1.1.3.
+    double buffer = maximumOrder*maximumStepSize; //see Tudat libraries 1.1.3.
     std::map< std::string, std::shared_ptr< BodySettings > > bodySettings;
     bodySettings = getDefaultBodySettings( bodyNames, initialEphemerisTime - buffer, finalEphemerisTime + buffer );
+
+    // Use direct SPICE ephemerides
+    if (useDirectSpice == true){
+        std::string frameOrigin = "SSB";
+        std::string frameOrientation = "ECLIPJ2000";
+        for( unsigned int i = 0; i < totalNumberOfBodies; i++ )
+        {
+            bodySettings[bodyNames[i]]->ephemerisSettings = std::make_shared< DirectSpiceEphemerisSettings >( frameOrigin, frameOrientation );
+        }
+    }
 
     // Custom settings Sun
     double sunNormalizedJ2 = sunJ2 / calculateLegendreGeodesyNormalizationFactor(2,0);
@@ -136,6 +159,8 @@ int main( )
     // Create body map
     NamedBodyMap bodyMap = createBodies( bodySettings );
     setGlobalFrameBodyEphemerides( bodyMap, "SSB", "ECLIPJ2000" );
+
+
 
 
     ///////////////////////
@@ -174,15 +199,41 @@ int main( )
     std::vector< std::string > centralBodies;
     centralBodies.resize( numberOfNumericalBodies );
 
-    // Set central body as Solar System Barycente for each body
+    // Set central body as Solar System Barycenter for Sun and each planet, for Moon use the Earth
     for( unsigned int i = 0; i < numberOfNumericalBodies; i++ )
     {
-        centralBodies[ i ] = "SSB";
+        if (bodiesToPropagate[i] == "moon"){
+            centralBodies[i] = "Earth";
+        } else{
+            centralBodies[i] = "SSB";
+        }
     }
 
     // Create acceleration models and propagation settings.
     AccelerationMap accelerationModelMap = createAccelerationModelsMap(
                 bodyMap, accelerationMap, bodiesToPropagate, centralBodies );
+
+
+
+
+    ////////////////////////////////
+    //// ESTIMATABLE PARAMETERS ////
+    ////////////////////////////////
+
+    std::vector< boost::shared_ptr < EstimatableParameterSettings > > parameterNames;
+
+    parameterNames.push_back(boost::make_shared<EstimatableParameterSettings >
+                             ("global_metric", ppn_parameter_gamma ) );
+    parameterNames.push_back(boost::make_shared<EstimatableParameterSettings >
+                             ("global_metric", ppn_parameter_beta ) );
+    parameterNames.push_back(boost::make_shared<EstimatableParameterSettings >
+                             ("Sun", gravitational_parameter));
+    parameterNames.push_back(boost::make_shared<SphericalHarmonicEstimatableParameterSettings>
+                             (2,0,2,0,"Sun",spherical_harmonics_cosine_coefficient_block));
+
+    std::shared_ptr< EstimatableParameterSet< double > > parametersToEstimate =
+         createParametersToEstimate( parameterNames, bodyMap, );
+
 
 
 
@@ -197,15 +248,29 @@ int main( )
     Eigen::VectorXd systemInitialState = getInitialStatesOfBodies(
                 bodiesToPropagate, centralBodies, bodyMap, initialEphemerisTime );
 
+//    // Define dependent variables to save
+//    std::vector< std::shared_ptr< SingleDependentVariableSaveSettings > > dependentVariablesList;
+//    dependentVariablesList.push_back(
+//        std::make_shared< SingleAccelerationDependentVariableSaveSettings >(
+//        accelerationModelType, bodyUndergoingAcceleration, bodyExertingAcceleration, useNorm, componentIndex );
+//    std::shared_ptr< DependentVariableSaveSettings > dependentVariablesToSave =
+//        std::make_shared< DependentVariableSaveSettings >( dependentVariablesList );
+//    https://tudat.tudelft.nl/tutorials/tudatFeatures/propagationSetup/propagatorSettingsDependentVariables.html
+
+
     // Define propagator settings.
     std::shared_ptr< TranslationalStatePropagatorSettings< double > > propagatorSettings =
             std::make_shared< TranslationalStatePropagatorSettings< double > >
-            ( centralBodies, accelerationModelMap, bodiesToPropagate, systemInitialState, finalEphemerisTime );
+            ( centralBodies, accelerationModelMap, bodiesToPropagate,
+              systemInitialState, finalEphemerisTime);
 
     // Define numerical integrator settings.
-    std::shared_ptr< IntegratorSettings< > > integratorSettings =
-            std::make_shared< IntegratorSettings< > >
-            ( rungeKutta4, initialEphemerisTime, integrationStepSize );
+    std::shared_ptr< AdamsBashforthMoultonSettings< double > > integratorSettings =
+            std::make_shared< AdamsBashforthMoultonSettings< double > > (
+                initialEphemerisTime, initialTimeStep,
+                minimumStepSize, maximumStepSize,
+                relativeErrorTolerence, absoluteErrorTolerence,
+                minimumOrder, maximumOrder);
 
 
 
@@ -217,9 +282,11 @@ int main( )
     std::cout << "propagating orbits..." << std::endl;
 
     // Create simulation object and propagate dynamics.
-    SingleArcDynamicsSimulator< > dynamicsSimulator( bodyMap, integratorSettings, propagatorSettings );
+    SingleArcDynamicsSimulator< > dynamicsSimulator(
+                bodyMap, integratorSettings, propagatorSettings );
 
-    std::map< double, Eigen::VectorXd > integrationResult = dynamicsSimulator.getEquationsOfMotionNumericalSolution( );
+    std::map< double, Eigen::VectorXd > integrationResult =
+            dynamicsSimulator.getEquationsOfMotionNumericalSolution( );
 
     // Retrieve numerically integrated state for each body.
     std::vector< std::map< double, Eigen::VectorXd > > allBodiesPropagationHistory;
@@ -249,14 +316,13 @@ int main( )
         // Write propagation history to file.
         input_output::writeDataMapToTextFile(
                     allBodiesPropagationHistory[ i ],
-                    "innerSolarSystemPropagationHistory" + bodyNames.at( i ) + ".dat",
+                    "innerSolarSystemPropagationHistory" + bodiesToPropagate.at( i ) + ".dat",
                     tudat_applications::getOutputPath( ) + outputSubFolder,
                     "",
                     std::numeric_limits< double >::digits10,
                     std::numeric_limits< double >::digits10,
                     "," );
     }
-
 
     // Final statement.
     // The exit code EXIT_SUCCESS indicates that the program was successfully executed.
